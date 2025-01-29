e861e849acb50cabcc9178daceb57174754a1146
emrekr <emre.kr@hotmail.com>
2025-01-29 18:30:58 +0300

    Domain layer created

---
+++ GeofenceSDK/Domain/GSGeofenceMonitoringUseCase.swift
@@ -0,0 +1,13 @@
+//
+//  GSGeofenceMonitoringUseCase.swift
+//  GeofenceExampleApp
+//
+//  Created by Emre Kuru on 29.01.2025.
+//
+
+import Foundation
+
+/// Protocol defining the geofence monitoring use case.
+protocol GSGeofenceMonitoringUseCase {
+    func startMonitoring()
+}
