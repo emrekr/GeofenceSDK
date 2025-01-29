b81cfa2c5ac11ab3f1a15014901899f237aaf93c
emrekr <emre.kr@hotmail.com>
2025-01-29 18:22:42 +0300

    project and its targets are created

---
+++ GeofenceExampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GeofenceExampleApp
+//
+//  Created by Emre Kuru on 29.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GeofenceSDK/GeofenceSDK.h
@@ -0,0 +1,18 @@
+//
+//  GeofenceSDK.h
+//  GeofenceSDK
+//
+//  Created by Emre Kuru on 29.01.2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GeofenceSDK.
+FOUNDATION_EXPORT double GeofenceSDKVersionNumber;
+
+//! Project version string for GeofenceSDK.
+FOUNDATION_EXPORT const unsigned char GeofenceSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GeofenceSDK/PublicHeader.h>
+
+
